3b11e0ddc612e10722a4880adfad02c4d36d8b34
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-02-21 10:58:35 +0300

    add tracking.js file

---
+++ tracking.js
@@ -0,0 +1,10 @@
+/*!
+ * reveal.js-tracking plugin v1.0.0
+ * MIT licensed
+ *
+ * Copyright (C) 2020 Joe Pantazidis
+ */
+
+var RevealTracking = window.RevealTracking || (function () {
+  var config = Reveal.getConfig().tracking;
+})();
